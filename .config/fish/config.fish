a838c388ea879d8e02e529e32fb9f621eb4e314d
arpan <arpan@123>
2024-03-30 15:44:15 +0530

    fish n zathura

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+set fish_greeting
+starship init fish | source
+
+alias nv 'nvim'
+alias rn 'ranger'
+
+alias ts 'tmux ls'
+alias tc 'tmux new-session -t'
+alias ta 'tmux attach-session -t'
+alias nifi 'nmcli device wifi connect'
