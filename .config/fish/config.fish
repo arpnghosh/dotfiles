a91658f3e49b6c52925d8503b6800ee2c572e487
Arpan Ghosh <arpnghosh@protonmail.com>
2024-04-01 09:29:32 +0530

    Added new Colorschemes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,3 +8,6 @@ alias ts 'tmux ls'
 alias tc 'tmux -u new-session -t'
 alias ta 'tmux -u attach-session -t'
 alias nifi 'nmcli device wifi connect'
+
+set PATH $PATH $HOME/.local/share/nvm/v20.12.0/bin
+set PATH $PATH fish_user_paths $HOME/.cargo/bin
