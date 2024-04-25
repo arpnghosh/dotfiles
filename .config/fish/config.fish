ece89393730b1bb9faf89b4b0f7e8d601d85ec35
Arpan Ghosh <arpnghosh@protonmail.com>
2024-04-25 10:29:00 +0530

    update: fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@ set fish_greeting
 starship init fish | source
 
 alias nv 'nvim'
+alias neo 'neovide --no-fork'
 alias rn 'ranger'
 
 alias ts 'tmux ls'
@@ -11,3 +12,7 @@ alias nifi 'nmcli device wifi connect'
 
 set PATH $PATH $HOME/.local/share/nvm/v20.12.0/bin
 set PATH $PATH fish_user_paths $HOME/.cargo/bin
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
