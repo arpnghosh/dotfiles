01685876460e6a0867bc48a66125ccd887d40884
Arpan Ghosh <arpnghosh@protonmail.com>
2024-09-01 10:11:31 +0530

    fishhhh

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,19 @@
 set fish_greeting
 starship init fish | source
 
+set -U fish_cursor_default line
+
+alias tmux="tmux -f ~/.config/tmux/tmux.conf"
 alias nv 'nvim'
 alias neo 'neovide --no-fork'
 alias rn 'ranger'
+alias zd 'zeditor'
+alias hx 'helix'
+alias ff 'firefox'
+alias zz 'zathura'
+
+alias la 'ls -a'
+alias ll 'ls -l'
 
 alias ts 'tmux ls'
 alias tc 'tmux -u new-session -t'
@@ -12,6 +22,8 @@ alias nifi 'nmcli device wifi connect'
 
 set PATH $PATH $HOME/.local/share/nvm/v20.12.0/bin
 set PATH $PATH fish_user_paths $HOME/.cargo/bin
+
+set -Ux fish_user_paths $HOME/.local/bin $fish_user_paths
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
