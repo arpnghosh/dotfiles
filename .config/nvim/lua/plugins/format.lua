eb29e65ffff4cd1ba33a5e8164fdac9c60254b32
arpnghosh <arpnghosh@protonmail.com>
2025-08-20 19:24:28 +0530

    nvim: bind format to a keymap

--- .config/nvim/lua/plugins/format.lua
+++ .config/nvim/lua/plugins/format.lua
@@ -2,13 +2,13 @@ return {
 	"stevearc/conform.nvim",
 	opts = {
 		notify_on_error = false,
-		format_on_save = function(bufnr)
-			local disable_filetypes = { c = true, cpp = true }
-			return {
-				timeout_ms = 500,
-				lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-			}
-		end,
+		-- format_on_save = function(bufnr)
+		-- 	local disable_filetypes = { c = true, cpp = true }
+		-- 	return {
+		-- 		timeout_ms = 500,
+		-- 		lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+		-- 	}
+		-- end,
 		formatters_by_ft = {
 			c = { "clang-format" },
 			cpp = { "clang-format" },
