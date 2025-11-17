ffa74f5c0646ff2fbcb1a5740a4372afb943de0c
Arpan Ghosh <arpnghosh@protonmail.com>
2025-11-18 01:15:25 +0530

    chore: update to match current setup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,16 +3,21 @@ set fish_greeting
 starship init fish | source
 
 alias v nvim
+alias z zeditor
 alias y yazi
 
-alias la 'ls -a'
-alias ll 'ls -l'
+alias ls 'eza --icons'
+alias la 'eza -a --icons'
+alias ll 'eza -l --icons'
+alias lla 'eza -la --icons'
 
 alias ts 'tmux ls'
 alias tc 'tmux -u new-session -t'
 alias ta 'tmux -u attach-session -t'
 
 alias nifi 'nmcli device wifi connect'
+
+alias sync-notes '~/Documents/notes/sync-notes.sh'
 
 # function vv
 #      set selected_dir (cd ~/Documents/code && ls -d */ | fzf)
@@ -37,12 +42,15 @@ function fish_vi_cursor --on-variable fish_bind_mode
     end
 end
 
+# nvm
 set PATH $PATH $HOME/.local/share/nvm/v20.12.0/bin
 set PATH $PATH fish_user_paths $HOME/.cargo/bin
 set -Ux fish_user_paths $HOME/.local/bin $fish_user_paths
 
+# emacs
 set -Ux fish_user_paths $HOME/.config/emacs/bin
 
+# lua
 set -Ux LUA_PATH "/usr/local/share/lua/5.4/?.lua;/usr/local/share/lua/5.4/?/init.lua;;"
 set -Ux LUA_CPATH "/usr/local/lib/lua/5.4/?.so;;"
 
@@ -51,4 +59,12 @@ set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
 # go
-set --export PATH $HOME/.go/bin $PATH
+set -x GOPATH $HOME/go
+set -x PATH $PATH $GOPATH/bin
+
+# pnpm
+set -gx PNPM_HOME "/home/awaki/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
